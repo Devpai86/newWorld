9583f3e3ee11864653860b3898a7dce8f4ba5f61
Devpai86 <110974251+Devpai86@users.noreply.github.com>
2022-08-11 17:40:14 +0530

    Add files via upload

---
+++ NewWorld/src/main/java/com/spring/Newworld/HomeController.java
@@ -0,0 +1,16 @@
+package com.spring.Newworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	
+	public String showHomePage()
+	{
+		System.out.println("Controller invoked");
+		return "home";
+	}
+
+}
